40189b32409ea81dfe02f567f810b322a0289287
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-08-12 15:53:28 +0700

    Update UserController.php
    Applying auth and deauth for users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,10 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
@@ -63,4 +67,70 @@ class UserController extends Controller
             ], 409);
         }
     }
+
+    public function authenticate(Request $request) {
+
+        $validator = Validator::make($request->all(),[
+        'email' => 'required',
+        'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => $validator->errors(),
+            ], 422);
+        }
+
+        $credentials =  $request->only('email', 'password');
+        $token = auth('api')->attempt($credentials);
+
+        if(!$token) {
+            return response()->json([
+                'success' => false,
+                'message'=> "Email atau Password salah"
+            ], 401);
+        } else {
+            return response()->json([
+                'success' => true,
+                'user' => auth('api')->user(),
+                'token' => $token, 
+            ], 200);
+        }
+    }
+
+    public function deauth(Request $request) {
+
+        try {
+            $token = JWTauth::getToken();
+            if(!$token) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Token tidak ditemukan',
+                ], 400);
+            } else {
+                auth('api')->logout(true);
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Logout berhasil',
+                ],200);
+            }
+        } catch(TokenExpiredException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token expired!',
+            ],401);
+        } catch(TokenInvalidException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token Invalid!',
+            ],401);
+        } catch(JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => "Token not found or can't be processed!",
+            ],401);
+        }
+    }
+
 }
