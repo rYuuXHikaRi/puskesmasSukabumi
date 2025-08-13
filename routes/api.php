98f9b9e2853e1e9bce1d0c47481047e9a273c862
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-08-13 16:57:44 +0700

    Update api.php
    Add route for CRU Medicine Storage

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\MedicineStorageController;
 use App\Http\Controllers\Api\MedicineUnitMeasurenmentController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
@@ -9,11 +10,18 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/user/show', [UserController::class, 'index'])->middleware('auth:api');
-Route::post('/user/register', [UserController::class, 'register'])->middleware('auth:api');
 Route::post('/auth', [UserController::class, 'authenticate']);
 Route::post('/user/deauth', [UserController::class, 'deauth']);
+Route::get('/user/show', [UserController::class, 'index'])->middleware('auth:api');
+Route::post('/user/register', [UserController::class, 'register'])->middleware('auth:api');
+
 
 Route::get('/medicine/unitmeasurement/show', [MedicineUnitMeasurenmentController::class, 'index'])->middleware('auth:api');
 Route::post('/medicine/unitmeasurement/register', [MedicineUnitMeasurenmentController::class, 'store'])->middleware('auth:api');
 Route::put('/medicine/unitmeasurement/edit/{id}', [MedicineUnitMeasurenmentController::class, 'update'])->middleware('auth:api');
+
+Route::get('/medicine/storage/show', [MedicineStorageController::class, 'index'])->middleware('auth:api');
+Route::post('/medicine/storage/register', [MedicineStorageController::class, 'store'])->middleware('auth:api');
+Route::put('/medicine/storage/edit/{id}', [MedicineStorageController::class, 'update'])->middleware('auth:api');
+
+
