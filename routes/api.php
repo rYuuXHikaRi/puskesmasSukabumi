d649b6d82b5facadbf513019a4151fce583c601a
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-08-13 16:12:43 +0700

    Update api.php
    Add route for CRUD Medicine Measurement Unit
    Change path for User CRUD Route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\MedicineUnitMeasurenmentController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,7 +9,11 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/index', [UserController::class, 'index'])->middleware('auth:api');
-Route::post('/register', [UserController::class, 'register']);
+Route::get('/user/show', [UserController::class, 'index'])->middleware('auth:api');
+Route::post('/user/register', [UserController::class, 'register'])->middleware('auth:api');
 Route::post('/auth', [UserController::class, 'authenticate']);
-Route::post('/deauth', [UserController::class, 'deauth']);
+Route::post('/user/deauth', [UserController::class, 'deauth']);
+
+Route::get('/medicine/unitmeasurement/show', [MedicineUnitMeasurenmentController::class, 'index'])->middleware('auth:api');
+Route::post('/medicine/unitmeasurement/register', [MedicineUnitMeasurenmentController::class, 'store'])->middleware('auth:api');
+Route::put('/medicine/unitmeasurement/edit/{id}', [MedicineUnitMeasurenmentController::class, 'update'])->middleware('auth:api');
