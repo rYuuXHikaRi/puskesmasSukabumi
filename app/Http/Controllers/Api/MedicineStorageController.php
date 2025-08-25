6a9393e6aeef97b516da3d53df69759dc804733a
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-08-25 11:07:20 +0700

    Update MedicineStorageController.php
    - add Medicine Stock on Warehouse management

--- app/Http/Controllers/Api/MedicineStorageController.php
+++ app/Http/Controllers/Api/MedicineStorageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Gudang;
+use App\Models\GudangObat;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -121,4 +122,169 @@ class MedicineStorageController extends Controller
             ], 401);
         }
     }
+
+    public function medicineStockIndex(Request $request) {
+        $gudangId = $request->input('gudangId');
+        $obatId = $request->input('obatId');
+        $gudang = Gudang::find($gudangId);
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if(!empty($gudangId) && !empty($obatId)) {
+                $medicineStock = $gudang->obat()->where('obat_id', $obatId)->first();
+            } else if(!empty($gudangId)) {
+                $medicineStock = GudangObat::where('gudang_id', $gudangId)
+                                            ->get();
+            } else if(!empty($obatId)) {
+                $medicineStock = GudangObat::where('obat_id', $obatId)
+                                            ->get();
+            } else {
+                $medicineStock = GudangObat::all();
+            }
+            
+            if ($medicineStock->isEmpty() || !$medicineStock) {
+                return response()->json(['message' => 'Data stok tidak ditemukan.'], 404);
+            } else {
+                return response()->json([
+                    'success' => true,
+                    'data' => $medicineStock
+                ], 200);
+            };
+        } catch(TokenExpiredException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token expired!',
+            ],401);
+        } catch(TokenInvalidException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token Invalid!',
+            ],401);
+        }
+
+    }
+
+    public function medicineStockStore(Request $request) {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            $validator = Validator::make($request->all(),[
+                'obat_id' => ['required', 'integer', Rule::exists('m_obat', 'id')],
+                'gudang_id' => ['required', 'integer', Rule::exists('m_gudang', 'id')],
+                'stok' => 'required|integer|min:0'
+            ]);
+
+            if($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Periksa data inputan kembali !'
+                ], 422);
+            };
+
+            $gudang = Gudang::find($request->gudang_id);
+            $isExist = $gudang->obat()->where('obat_id', $request->obat_id)->first();
+           
+            if($isExist) {
+                return response()->json([
+                    'message' => 'Entri stok untuk obat ini di gudang sudah ada. Gunakan PUT untuk memperbarui.',
+                    'data' => $isExist->pivot
+                ], 409);
+            } else {
+                
+                // $medicineStock = GudangObat::create([
+                //     'obat_id' => $request->obat_id,
+                //     'gudang_id' => $request->gudang_id,
+                //     'stok' => $request->stok
+                // ]);
+
+                
+                $gudang->obat()->attach($request->obat_id, ['stok' => $request->stok]);
+                $medicineStock = $gudang->obat()->where('obat_id', $request->obat_id)->first();
+                
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Entry Stok Obat berhasil ditambahkan',
+                    'data' => $medicineStock->pivot,
+                ], 201);
+            }
+
+        } catch(\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal menambahkan data stock gudang, Periksa input kembali !',
+            ], 422);
+        } catch(TokenExpiredException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token Expired ! Silahkan re:login kembali',
+            ], 401);
+        } catch(TokenInvalidException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token Invalid ! Silahkan re:login kembali',
+            ],401);
+        }
+    }
+
+    public function medicineStockUpdate(Request $request) {
+        $gudangId = $request->input('gudangId');
+        $obatId = $request->input('obatId');
+        $gudang = Gudang::find($gudangId);
+
+        $validator = Validator::make($request->all(),[
+            'stok' => 'required|integer|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal memperbarui data stok, periksa input kembali!',
+                'errors' => $validator->errors() // Ambil semua error validasi
+            ], 422);
+        }
+
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            $medicineStock = GudangObat::where('obat_id', $obatId)
+                                    ->where('gudang_id', $gudangId)
+                                    ->first();
+            if (!$medicineStock) {
+                return response()->json([
+                    'message' => 'Data stok tidak ditemukan.'
+                ],404);
+            } else {
+
+                $gudang->obat()->updateExistingPivot($obatId, [
+                    'stok' => $request->stok,
+                ]);
+                $medicineStock = $gudang->obat()->where('obat_id', $obatId)->first();
+
+                return response()->json([
+                    'message' => 'Stok gudang obat berhasil diperbarui.',
+                    'data' => $medicineStock->pivot
+                ], 200);
+            }
+
+        } catch(\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal memperbaharui data stock obat di gudang, Periksa input kembali !',
+            ], 422);
+        } catch(TokenExpiredException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token Expired ! Silahkan re:login kembali',
+            ], 401);
+        } catch(TokenInvalidException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token Invalid ! Silahkan re:login kembali',
+            ],401);
+        }
+    }
+
+    public function medicineStockGet() {
+        
+    }
+
+
 }
